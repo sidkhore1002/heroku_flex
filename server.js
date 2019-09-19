413d9eb3237efb60fb31803b61b48bed3f156ce0
Siddharth Khore <siddharth.khore@iauro.com>
2019-09-19 16:28:37 +0530

    3rd commit

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src'));
+res.sendFile(path.join(__dirname+'/dist'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
